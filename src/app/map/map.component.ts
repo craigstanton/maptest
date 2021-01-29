fe1a29c6b0c231afadc91df36cba4da7c01ebb14
Joshua Gore <joshua_gore@eagle.co.nz>
2021-01-29 14:46:46 +1300

    updated to new versions part of release of Esri PAAS solution

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import * as esriVector from 'esri-leaflet-vector';
-import * as esriVector from '../../esri-leaflet-vector/esri-leaflet-vector.js';
+import * as esriVector from 'esri-leaflet-vector';
 import * as L from 'leaflet';
 
 @Component({
@@ -14,12 +13,7 @@ export class MapComponent implements OnInit {
 
   ngOnInit() {
     const map = L.map('map').setView([-41.09, 174.88], 5);
-    console.log(esriVector);
-    // my working map
-    // const vectorTileLayer = esriVector.layer('b8cccf2315944c0c885e3150294067f2');
-    // other stuff
-    // const vectorTileLayer = esriVector.layer('573ba47eb6894cd6a4353799382fdd1d');
-    const vectorTileLayer = esriVector.layer('30e465e2b5164dcb9a5b9f784c2baffc');
+    const vectorTileLayer = esriVector.vectorTileLayer('018ac5f6efbb4dc98c7239f58476b7de');
     map.addLayer(vectorTileLayer);
   }
 
