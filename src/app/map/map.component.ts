4b5628e03542fd510c7398bedbcc564cb608287a
Joshua Gore <joshua_gore@eagle.co.nz>
2021-02-18 09:48:20 +1300

    updated to use combination of vector and raster maps

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as esriVector from 'esri-leaflet-vector';
+import * as esriLeaflet from 'esri-leaflet';
 import * as L from 'leaflet';
 
 @Component({
@@ -13,8 +14,37 @@ export class MapComponent implements OnInit {
 
   ngOnInit() {
     const map = L.map('map').setView([-41.09, 174.88], 5);
-    const vectorTileLayer = esriVector.vectorTileLayer('018ac5f6efbb4dc98c7239f58476b7de');
-    map.addLayer(vectorTileLayer);
+    // hillshade
+    var hillshadeLayer = esriLeaflet.tiledMapLayer({
+      url: 'https://server.arcgisonline.com/arcgis/rest/services/Elevation/World_Hillshade/MapServer',
+      maxNativeZoom: 13
+    });
+    hillshadeLayer.addTo(map);
+    // raster toplevel
+    var rasterBasemap = esriLeaflet.tiledMapLayer({
+      url: 'https://tiles.arcgis.com/tiles/hLRlshaEMEYQG5A8/arcgis/rest/services/niwa_png_nz_0_13_v2/MapServer',
+      maxZoom: 13
+    });
+    rasterBasemap.addTo(map);
+    // vector detailed
+    var vectorBasemap = esriVector.vectorTileLayer('93b8c97a705340fc8c12ec0c2ab95379', {minZoom: 13});
+    // vectorBasemap.addTo(map);
+    map.on('zoomend', function () {
+      var zoomLevel = map.getZoom();
+      console.log(`zoom is ${zoomLevel}`);
+      if (zoomLevel >= 13) {
+        if (!map.hasLayer(vectorBasemap)){
+          console.log('adding layer');
+          vectorBasemap.addTo(map);
+        }
+      }
+      else {
+        if (map.hasLayer(vectorBasemap)) {
+          console.log('removing layer');
+          vectorBasemap.remove();
+        }
+      }
+    })
   }
 
 }
