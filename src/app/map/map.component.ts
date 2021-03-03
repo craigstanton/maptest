7b68b05c5c52b50d46a18dfaf5401cc466f12c0f
Joshua Gore <joshua_gore@eagle.co.nz>
2021-03-03 14:05:51 +1300

    added maps for only hillshade and vector basemap to test attribution

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -14,6 +14,8 @@ export class MapComponent implements OnInit {
 
   ngOnInit() {
     const map = L.map('map').setView([-41.09, 174.88], 5);
+    const map2 = L.map('map2').setView([-41.09, 174.88], 5);
+    const map3 = L.map('map3').setView([-41.09, 174.88], 5);
     // hillshade
     var hillshadeLayer = esriLeaflet.tiledMapLayer({
       // url: 'https://server.arcgisonline.com/arcgis/rest/services/Elevation/World_Hillshade/MapServer',
@@ -21,6 +23,12 @@ export class MapComponent implements OnInit {
       maxNativeZoom: 13
     });
     hillshadeLayer.addTo(map);
+    var hillshadeLayer2 = esriLeaflet.tiledMapLayer({
+      // url: 'https://server.arcgisonline.com/arcgis/rest/services/Elevation/World_Hillshade/MapServer',
+      url: 'https://services.arcgisonline.com/arcgis/rest/services/Elevation/World_Hillshade/MapServer',
+      maxNativeZoom: 13
+    });
+    hillshadeLayer2.addTo(map2);
     // raster toplevel
     var rasterBasemap = esriLeaflet.tiledMapLayer({
       // url: 'https://tiles.arcgis.com/tiles/hLRlshaEMEYQG5A8/arcgis/rest/services/niwa_png_nz_0_13_v2/MapServer',
@@ -29,6 +37,13 @@ export class MapComponent implements OnInit {
       maxZoom: 13
     });
     rasterBasemap.addTo(map);
+    var rasterBasemap2 = esriLeaflet.tiledMapLayer({
+      // url: 'https://tiles.arcgis.com/tiles/hLRlshaEMEYQG5A8/arcgis/rest/services/niwa_png_nz_0_13_v2/MapServer',
+      // url: 'https://tiles.arcgis.com/tiles/fp1tibNcN9mbExhG/arcgis/rest/services/NIWA_Weather_Basemap/MapServer',
+      url: 'https://tiles.arcgis.com/tiles/hLRlshaEMEYQG5A8/arcgis/rest/services/niwa_contours_v3_credits/MapServer',
+      maxZoom: 13
+    });
+    rasterBasemap2.addTo(map3);
     // vector detailed
     var vectorBasemap = esriVector.vectorTileLayer('93b8c97a705340fc8c12ec0c2ab95379', {minZoom: 13});
     // vectorBasemap.addTo(map);
